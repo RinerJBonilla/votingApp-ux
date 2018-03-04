5ab65706a7a301cd0850d2086898a6a0e719fa33
RinerJBonilla <lafoepalisades@hotmail.com>
2018-03-04 12:31:57 -0600

    updating state, i guess

--- public/js/app.js
+++ public/js/app.js
@@ -4,13 +4,29 @@ class ProductList extends React.Component {
     this.state = {
     products: [],
     };
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
     }
+
     componentDidMount() {
       this.setState({ products: Seed.products });
       }
-  handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
-    }
+
+      handleProductUpVote(productId) {
+        console.log(productId + ' was upvoted.');
+        const nextProducts = this.state.products.map((product) => {
+        if (product.id === productId) {
+        return Object.assign({}, product, {
+        votes: product.votes + 1,
+        });
+        } else {
+        return product;
+        }
+        });
+        this.setState({
+        products: nextProducts,
+        });
+        }
+
   render() {
     const products = this.state.products.sort((a, b) => (
       b.votes - a.votes
@@ -41,7 +57,7 @@ class Product extends React.Component {
     super(props);
     this.handleUpVote = this.handleUpVote.bind(this);
     }
-  handleUpVote() {
+  handleUpVote = () => {
     this.props.onVote(this.props.id);
     }
   render() {
