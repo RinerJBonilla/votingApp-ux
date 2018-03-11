895a54b9c2dbd5c91baa9547e9f2f0aa8be33747
RinerJBonilla <lafoepalisades@hotmail.com>
2018-03-11 12:50:22 -0600

    Project Complete-te

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,13 @@
 class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      products: [],
+  state = {
+    products: [],
     };
-
-    this.handleProductUpVote = this.handleProductUpVote.bind(this);
-  }
 
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
 
-  handleProductUpVote(productId) {
+  handleProductUpVote = (productId) => {
     const nextProducts = this.state.products.map((product) => {
     if (product.id === productId) {
     return Object.assign({}, product, {
@@ -63,7 +57,7 @@ class Product extends React.Component {
 
     this.handleUpVote = this.handleUpVote.bind(this);
   }
-  handleUpVote() {
+  handleUpVote = () => {
     this.props.onVote(this.props.id);
   }
   render() {
